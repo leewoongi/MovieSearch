d40b11dc64c5342ae50bb632fb971f54c820c18f
leewoongi <t2korea@naver.com>
2021-02-17 19:56:52 +0900

    feat #16 : 웹뷰 수정

--- app/src/main/java/com/experiencers/ui/movie/MovieWebView.kt
+++ app/src/main/java/com/experiencers/ui/movie/MovieWebView.kt
@@ -1,19 +1,22 @@
 package com.experiencers.ui.movie
 
 import android.os.Bundle
+import android.webkit.WebViewClient
 import com.experiencers.base.BaseActivity
 import com.experiencers.ui.R
 import com.experiencers.ui.databinding.ActivityMovieWebviewBinding
 
-class MovieWebView: BaseActivity<ActivityMovieWebviewBinding>() {
+class MovieWebView : BaseActivity<ActivityMovieWebviewBinding>() {
     override fun layoutRes(): Int = R.layout.activity_movie_webview
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val webViewUrl: String? = intent.getStringExtra("url")
-        if (webViewUrl != null) {
-            binding.wv.loadUrl(webViewUrl)
+        binding.wv.webViewClient = WebViewClient()
+        binding.wv.settings.javaScriptEnabled = true
+
+        intent.getStringExtra("url")?.let {
+            binding.wv.loadUrl(it)
         }
     }
 }
